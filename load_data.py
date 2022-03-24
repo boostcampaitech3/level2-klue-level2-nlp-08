e67c25c3f03522a8c9e775ec838ff28a510deee2
root <mooncy0421>
2022-03-24 03:55:45 +0000

    FEAT: Entity Token 추가
    preprocessing_dataset에 Entity token을 special token으로
    추가
    tokenized_dataset에 sentence앞 entity 붙이는것 삭제

--- load_data.py
+++ load_data.py
@@ -22,13 +22,35 @@ def preprocessing_dataset(dataset):
   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
   subject_entity = []
   object_entity = []
-  for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
+  # for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
+  #   i = i[1:-1].split(',')[0].split(':')[1]
+  #   j = j[1:-1].split(',')[0].split(':')[1]
+
+  #   subject_entity.append(i)
+  #   object_entity.append(j)
+  # out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
+
+  print("NEW PREPROCESSING")
+  sentence = []
+  for i,j, sent, ids in zip(dataset['subject_entity'], dataset['object_entity'], dataset['sentence'], dataset['id']):
+    i_start = int(i.split('\':')[2].split(',')[0])
+    i_end = int(i.split('\':')[3].split(',')[0])
+    j_start = int(j.split('\':')[2].split(',')[0])
+    j_end = int(j.split('\':')[3].split(',')[0])
+
     i = i[1:-1].split(',')[0].split(':')[1]
     j = j[1:-1].split(',')[0].split(':')[1]
-
+    
+    #sent = sent[:i_start] + '[SUBJ]' + sent[i_start:i_end+1] + '[/SUBJ]' + sent[i_end+1:j_start] + '[OBJ]' + sent[j_start:j_end+1] + '[/OBJ]' + sent[j_end+1:]
+    if i_start < j_start:
+      result = sent[:i_start] + '[SUBJ]' + sent[i_start:i_end+1] + '[/SUBJ]' + sent[i_end+1:j_start] + '[OBJ]' + sent[j_start:j_end+1] + '[/OBJ]' + sent[j_end+1:]
+    else:
+      result = sent[:j_start] + '[OBJ]' + sent[j_start:j_end+1] + '[/OBJ]' + sent[j_end+1:i_start] + '[SUBJ]' + sent[i_start:i_end+1] + '[/SUBJ]' + sent[i_end+1:]
+    
     subject_entity.append(i)
     object_entity.append(j)
-  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
+    sentence.append(result)
+  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':sentence,'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
   return out_dataset
 
 def load_data(dataset_dir):
@@ -40,13 +62,16 @@ def load_data(dataset_dir):
 
 def tokenized_dataset(dataset, tokenizer):
   """ tokenizer에 따라 sentence를 tokenizing 합니다."""
-  concat_entity = []
-  for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
-    temp = ''
-    temp = e01 + '[SEP]' + e02
-    concat_entity.append(temp)
+  # concat_entity = []
+  # for e01, e02 in zip(dataset['subject_entity'], dataset['object_entity']):
+  #   temp = ''
+  #   temp = e01 + '[SEP]' + e02
+  #   concat_entity.append(temp)
+
+  add_special = tokenizer.add_special_tokens({'additional_special_tokens':['[SUBJ]', '[/SUBJ]', '[OBJ]', '[/OBJ]']})
+  # tokenizer.__call__
   tokenized_sentences = tokenizer(
-      concat_entity,
+      # concat_entity,
       list(dataset['sentence']),
       return_tensors="pt",
       padding=True,
