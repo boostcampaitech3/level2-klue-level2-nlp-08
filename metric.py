9535871c1e88453c2703e1cc047c31f458e7826c
DJ_IM <gitvideouser@gmail.com>
2022-03-28 10:25:44 +0900

    데이터 복구

--- metric.py
+++ metric.py
@@ -65,13 +65,6 @@ def compute_metrics(pred):
       'accuracy': acc,
   }
 
-
-def calc_accuracy(X,Y):
-    max_vals, max_indices = torch.max(X, 1)
-    train_acc = (max_indices == Y).sum().data.cpu().numpy()/max_indices.size()[0]
-    return train_acc
-
-
 class MyTrainer(Trainer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
