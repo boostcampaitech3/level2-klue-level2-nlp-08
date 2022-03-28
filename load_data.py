978d29f6c5c798dbf60fcaa1388ee8a40b0811c6
root <mooncy0421>
2022-03-28 07:36:56 +0000

    REFACTOR : preprocessing_dataset refactoring
    Entity token 추가 함수 삽입

--- load_data.py
+++ load_data.py
@@ -1,7 +1,9 @@
+from email.policy import default
 import pickle as pickle
 import os
 import pandas as pd
 import torch
+from add_entity_token import *
 
 
 class RE_Dataset(torch.utils.data.Dataset):
@@ -20,33 +22,11 @@ class RE_Dataset(torch.utils.data.Dataset):
 
 def preprocessing_dataset(dataset):
   """ 처음 불러온 csv 파일을 원하는 형태의 DataFrame으로 변경 시켜줍니다."""
-  subject_entity = []
-  object_entity = []
-  # for i,j in zip(dataset['subject_entity'], dataset['object_entity']):
-  #   i = i[1:-1].split(',')[0].split(':')[1]
-  #   j = j[1:-1].split(',')[0].split(':')[1]
-
-  #   subject_entity.append(i)
-  #   object_entity.append(j)
-  # out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':dataset['sentence'],'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
-
-  print("NEW PREPROCESSING")
-  sentence = []
-  for i,j, sent, ids in zip(dataset['subject_entity'], dataset['object_entity'], dataset['sentence'], dataset['id']):
-    i_start = int(i.split('\':')[2].split(',')[0])
-    i_end = int(i.split('\':')[3].split(',')[0])
-    j_start = int(j.split('\':')[2].split(',')[0])
-    j_end = int(j.split('\':')[3].split(',')[0])
-    
-    if i_start < j_start:
-      result = sent[:i_start] + '[SUBJ]' + sent[i_start:i_end+1] + '[/SUBJ]' + sent[i_end+1:j_start] + '[OBJ]' + sent[j_start:j_end+1] + '[/OBJ]' + sent[j_end+1:]
-    else:
-      result = sent[:j_start] + '[OBJ]' + sent[j_start:j_end+1] + '[/OBJ]' + sent[j_end+1:i_start] + '[SUBJ]' + sent[i_start:i_end+1] + '[/SUBJ]' + sent[i_end+1:]
-    
-    subject_entity.append(sent[i_start:i_end+1])
-    object_entity.append(sent[j_start:j_end+1])
-    sentence.append(result)
-  out_dataset = pd.DataFrame({'id':dataset['id'], 'sentence':sentence,'subject_entity':subject_entity,'object_entity':object_entity,'label':dataset['label'],})
+  out_dataset = default_entity(dataset)
+  out_dataset = add_entity_token(dataset)
+  out_dataset = add_entity_typed_token(dataset)
+  out_dataset = swap_entity_typed_token(dataset)
+  
   return out_dataset
 
 def load_data(dataset_dir):
@@ -64,7 +44,15 @@ def tokenized_dataset(dataset, tokenizer):
   #   temp = e01 + '[SEP]' + e02
   #   concat_entity.append(temp)
 
-  add_special = tokenizer.add_special_tokens({'additional_special_tokens':['[SUBJ]', '[/SUBJ]', '[OBJ]', '[/OBJ]']})
+  added_special = tokenizer.add_special_tokens({'additional_special_tokens':['[SUBJ:PER]',  
+                                                                             '[SUBJ:ORG]',  
+                                                                             '[OBJ:PER]', 
+                                                                             '[OBJ:ORG]', 
+                                                                             '[OBJ:LOC]', 
+                                                                             '[OBJ:POH]', 
+                                                                             '[OBJ:NOH]', 
+                                                                             '[OBJ:DAT]', 
+                                                                             '[SUBJ]', '[/SUBJ]', '[OBJ]', '[/OBJ]']})
   # tokenizer.__call__
   tokenized_sentences = tokenizer(
       #concat_entity,
@@ -75,4 +63,4 @@ def tokenized_dataset(dataset, tokenizer):
       max_length=256,
       add_special_tokens=True,
       )
-  return tokenized_sentences
+  return tokenized_sentences, added_special
