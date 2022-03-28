d48397f950de6fc93d3d5d313747c223b0bbb1d3
mooncy0421 <co1504@naver.com>
2022-03-28 12:51:13 +0000

    REFACTOR: Method 이름 변ê 및 가독성 향상
    Method 이름 변경
    new_text에 f-string으로 가독성 향상
    add_entity_token.py의 method 병합

--- utils.py
+++ utils.py
@@ -12,43 +12,92 @@ def seed_everything(seed):
     np.random.seed(seed)
     random.seed(seed)
 mecab = Mecab()
+
 def add_spTok(text):
     for noun in mecab.nouns(text):
         text = text.replace(noun,'[NER]'+noun+'[/NER]')
     return text
-TYPE = {'ORG':'단체정보','PER':'사람정보','DAT':'날짜정보','LOC':'위치정보','POH':'기타정보','NOH':'기타 수량표현'}
-def add_punct(text, i_start, i_end, i_type,j_start, j_end, j_type):
-    if i_start < j_start:
-        # new_text = text[:i_start] + '*' + text[i_start:i_end + 1] + '[' + TYPE[i_type] + ']*' + text[i_end + 1:j_start] + '*' + \
+
+# *entity[TP]TYPE[/TP]*  * entity[TP]TYPE[/TP]*
+def add_entity_type_suffix_kr(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
+    TYPE = {'ORG':'단체정보','PER':'사람정보','DAT':'날짜정보','LOC':'위치정보','POH':'기타정보','NOH':'기타 수량표현'}
+    subj_word = text[subj_start:subj_end + 1]
+    obj_word = text[obj_start:obj_end + 1]
+    
+    if subj_start < obj_start:
+        # new_text = text[:subj_start] + '*' + text[subj_start:subj_end + 1] + '[' + TYPE[subj_type] + ']*' + text[subj_end + 1:j_start] + '*' + \
         #        text[j_start:j_end + 1] + '[' + TYPE[j_type] + ']*'  + text[j_end + 1:]
-        new_text = text[:i_start] + '*' + text[i_start:i_end + 1] + '[TP]' + TYPE[i_type] + '[/TP]*' + text[i_end + 1:j_start] + '*' + \
-                   text[j_start:j_end + 1] + '[TP]' + TYPE[j_type] + '[/TP]*' + text[j_end + 1:]
+        new_text = text[:subj_start] + f'*{subj_word}[TP]{TYPE[subj_type]}[/TP]*' + text[subj_end + 1:obj_start] + \
+                   f'*{obj_word}[TP]{TYPE[obj_type]}[/TP]*' + text[obj_end + 1:]
     else:
-        # new_text = text[:j_start] + '*' + text[j_start:j_end + 1] + '[' + TYPE[j_type] + ']*' + text[j_end + 1:i_start] + '*' + \
-        #        text[i_start:i_end + 1] + '[' + TYPE[i_type] + ']*' + text[i_end + 1:]
-        new_text = text[:j_start] + '*' + text[j_start:j_end + 1] + '[TP]' + TYPE[j_type] + '[/TP]*' + text[j_end + 1:i_start] + '*' + \
-                   text[i_start:i_end + 1] + '[TP]' + TYPE[i_type] + '[/TP]*' + text[i_end + 1:]
+        # new_text = text[:j_start] + '*' + text[j_start:j_end + 1] + '[' + TYPE[j_type] + ']*' + text[j_end + 1:subj_start] + '*' + \
+        #        text[subj_start:subj_end + 1] + '[' + TYPE[subj_type] + ']*' + text[subj_end + 1:]
+        new_text = text[:obj_start] + f'*{obj_word}[TP]{TYPE[obj_type]}[/TP]*' + text[obj_end + 1:subj_start] + \
+                   f'*{subj_word}[TP]{TYPE[subj_type]}[/TP]*' + text[subj_end + 1:]
     return new_text
-def typed_entity_marker(text, i_start, i_end, i_type, j_start, j_end, j_type):
-    new_text = ''
+
+# @*TYPE*entity@  #^TYPE^entity#
+def add_entity_type_punct_kr(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
     TYPE = {'ORG':'단체', 'PER':'사람', 'DAT':'날짜', 'LOC':'위치', 'POH':'기타', 'NOH':'수량'}
-    if i_start < j_start:
-      new_text = text[:i_start] + '@' + '*' + TYPE[i_type] + '*' + text[i_start:i_end + 1] + '@' + \
-                 text[i_end + 1:j_start] + '#' + '^' + TYPE[j_type] + '^' + text[j_start:j_end + 1] + '#' + text[j_end + 1:]
+    subj_word = text[subj_start:subj_end + 1]
+    obj_word = text[obj_start:obj_end + 1]
+    
+    if subj_start < obj_start:
+      new_text = text[:subj_start] + f'@*{TYPE[subj_type]}*{subj_word}@' + text[subj_end + 1:obj_start] + \
+                 f'#^{TYPE[obj_type]}^{obj_word}#' + text[obj_end + 1:]
     else:
-      new_text = text[:j_start] + '@' + '*' + TYPE[j_type] + '*' + text[j_start:j_end + 1] + '@' + \
-                 text[j_end + 1:i_start] + '#' + '^' + TYPE[i_type] + '^' + text[i_start:i_end + 1] + '#' + text[i_end + 1:]
+      new_text = text[:obj_start] + f'@*{TYPE[obj_type]}*{obj_word}@' + text[obj_end + 1:subj_start] + \
+                 f'#^{TYPE[subj_type]}^{subj_word}#' + text[subj_end + 1:]
     
     return new_text
 
-def entity_marker(text, i_start, i_end, i_type, j_start, j_end, j_type):
-    new_text = ''
-    TYPE = {'ORG':'단체', 'PER':'사람', 'DAT':'날짜', 'LOC':'위치', 'POH':'기타', 'NOH':'수량'}
-    if i_start < j_start:
-        new_text = text[:i_start] + '[' + i_type + ']' + text[i_start:i_end + 1] + '[/' + i_type + ']' \
-                + text[i_end + 1:j_start] + '[' + j_type + ']' + text[j_start:j_end + 1] + '[/' + j_type + ']' + text[j_end + 1:]
+# [subj_type]entity[/subj_type]  [obj_type]entity[/obj_type]
+def add_entity_type_token(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
+    subj_word = text[subj_start:subj_end + 1]
+    obj_word = text[obj_start:obj_end + 1]
+    
+    if subj_start < obj_start:
+        new_text = text[:subj_start] + f'[{subj_type}]{subj_word}[/{subj_type}]' + text[subj_end + 1:obj_start] + \
+                   f'[{obj_type}]{obj_word}[/{obj_type}]' + text[obj_end + 1:]
     else:
-        new_text = text[:j_start] + '[' + j_type + ']' + text[j_start:j_end + 1] + '[/' + j_type + ']' + \
-                 text[j_end + 1:i_start] + '[' + i_type + ']' + text[i_start:i_end + 1] + '[/' + i_type + ']' + text[i_end + 1:]
+        new_text = text[:obj_start] + f'[{obj_type}]{obj_word}[/{obj_type}]' + text[obj_end + 1:subj_start] + \
+                   f'[{subj_type}]{subj_word}[/{subj_type}]' + text[subj_end + 1:]
     
     return new_text
+
+
+
+# [SUBJ]entity[/SUBJ]
+def add_entity_token(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
+  subj_word = text[subj_start:subj_end + 1]
+  obj_word = text[obj_start:obj_end + 1]
+  
+  if subj_start < obj_start:
+    new_text = text[:subj_start] + f'[SUBJ]{subj_word}[/SUBJ]' + text[subj_end+1:obj_start] + f'[OBJ]{obj_word}[/OBJ]' + text[obj_end+1:]
+  else:
+    new_text = text[:obj_start] + f'[OBJ]{obj_word}[/OBJ]' + text[obj_end+1:subj_start] + f'[SUBJ]{subj_word}[/SUBJ]' + text[subj_end+1:]
+  return new_text
+
+
+# [SUBJ:type]entity[/SUBJ]
+def add_entity_token_with_type(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
+  subj_word = text[subj_start:subj_end + 1]
+  obj_word = text[obj_start:obj_end + 1]
+
+  if subj_start < obj_start:
+    new_text = text[:subj_start] + f'[SUBJ:{subj_type}]{subj_word}[/SUBJ]' + text[subj_end+1:obj_start] + f'[OBJ:{obj_type}]{obj_word}[/OBJ]' + text[obj_end+1:]
+  else:
+    new_text = text[:obj_start] + f'[OBJ:{obj_type}]{obj_word}[/OBJ]' + text[obj_end+1:subj_start] + f'[SUBJ:{subj_type}]{subj_word}[/SUBJ]' + text[subj_end+1:]
+  return new_text
+
+
+# entity --> [SUBJ:type]
+def swap_entity_token_with_type(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):    
+  if subj_start < obj_start:
+    new_text = text[:subj_start] + f'[SUBJ:{subj_type}]' + text[subj_end+1:obj_start] + f'[OBJ:{obj_type}]' + text[obj_end+1:]
+  else:
+    new_text = text[:obj_start] + f'[OBJ:{obj_type}]' + text[obj_end+1:subj_start] + f'[SUBJ:{subj_type}]' + text[subj_end+1:]
+  return new_text
+
+def default_sent(text, subj_start, subj_end, subj_type, obj_start, obj_end, obj_type):
+    return text
