cdc91f518b5777e58025762269eb0788521042ff
mooncy0421 <co1504@naver.com>
2022-03-30 13:34:18 +0000

    FIX: import path, config path 수정

--- trainer/main.py
+++ trainer/main.py
@@ -1,10 +1,10 @@
 import json
-from trainer.ensemble_train import ensemble_train
-from trainer.valid_train import valid_train
-from trainer.train_base import train_base
+from ensemble_train import ensemble_train
+from valid_train import valid_train
+from train_base import train_base
 
 def main():
-  with open('config.json', 'r') as f:
+  with open('./trainer/config.json', 'r') as f:
     config = json.load(f)
 
     if config['train_type'] == 'valid_train':
